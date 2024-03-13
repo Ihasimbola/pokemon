ff12832f4467f94ea7f6f9ff24e82fb5143f75a5
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2024-03-13 16:33:56 +0300

    feat : update the instaciation of pokemonService

--- controllers/pokemonController.ts
+++ controllers/pokemonController.ts
@@ -1,69 +1,70 @@
-import { Request, Response } from "express";
-import Users from "../entity/userEntity";
-import PokemonService from "../services/pokemonService";
+import { Request, Response } from 'express';
+import Users from '../entity/userEntity';
+import PokemonService from '../services/pokemonService';
 
 export class PokemonController {
-  static async getAll(req: Request, res: Response) {
-    const pokemonService = new PokemonService();
-    try {
-      const data = await pokemonService.getAll();
-      return res.status(200).json({
-        data,
-      });
-    } catch (error: any) {
-      throw new Error("Error getting all pokemon " + error.message);
-    }
-  }
-  static async catch(req: Request, res: Response) {
-    try {
-      const pokemonId = req.params.id;
-      const user = await Users.findById(req.body.userId);
-      if (!user) {
-        return res.status(401).json({ message: "User not found" });
-      }
-      const cachedPokemon = user?.cachedPokemon;
-      if (cachedPokemon?.includes(pokemonId)) {
-        return res
-          .status(200)
-          .json({ message: "Already catched", id: pokemonId });
-      }
-      user?.cachedPokemon?.push(pokemonId);
+	static pokemonService: PokemonService = new PokemonService();
 
-      user?.save();
-      return res.status(200).json({ message: "Catched" });
-    } catch (error: any) {
-      throw new Error("Error catching pokemon " + error.message);
-    }
-  }
+	static async getAll(req: Request, res: Response) {
+		try {
+			const data = await PokemonController.pokemonService.getAll();
+			return res.status(200).json({
+				data,
+			});
+		} catch (error: any) {
+			throw new Error('Error getting all pokemon ' + error.message);
+		}
+	}
+	static async catch(req: Request, res: Response) {
+		try {
+			const pokemonId = req.params.id;
+			const user = await Users.findById(req.body.userId);
+			if (!user) {
+				return res.status(401).json({ message: 'User not found' });
+			}
+			const cachedPokemon = user?.cachedPokemon;
+			if (cachedPokemon?.includes(pokemonId)) {
+				return res
+					.status(200)
+					.json({ message: 'Already catched', id: pokemonId });
+			}
+			user?.cachedPokemon?.push(pokemonId);
 
-  static async release(req: Request, res: Response) {
-    try {
-      const pokemonId = req.params.id;
-      const user = await Users.findById(req.body.userId);
-      if (!user) {
-        return res.status(401).json({ message: "User not found" });
-      }
-      const indexOfPokemonId = user?.cachedPokemon?.indexOf(pokemonId);
-      if (indexOfPokemonId) {
-        user.cachedPokemon?.splice(indexOfPokemonId, 1);
-        user.save();
-        return res.status(200).json({ message: "Released", id: pokemonId });
-      }
-      return;
-    } catch (error) {
-      throw new Error("Error releasing pokemon " + error);
-    }
-  }
+			user?.save();
+			return res.status(200).json({ message: 'Catched' });
+		} catch (error: any) {
+			throw new Error('Error catching pokemon ' + error.message);
+		}
+	}
 
-  static async getPokemonById(req: Request, res : Response) {
-    const pokemonService = new PokemonService();
-    try {
-        const pokemonId = parseInt(req.params.id);
-        const pokemon = await pokemonService.getById(pokemonId)
-        return res.json(pokemon);
-      } catch (error) {
-        throw new Error("Error releasing pokemon " + error);
-      }
+	static async release(req: Request, res: Response) {
+		try {
+			const pokemonId = req.params.id;
+			const user = await Users.findById(req.body.userId);
+			if (!user) {
+				return res.status(401).json({ message: 'User not found' });
+			}
+			const indexOfPokemonId = user?.cachedPokemon?.indexOf(pokemonId);
+			if (indexOfPokemonId) {
+				user.cachedPokemon?.splice(indexOfPokemonId, 1);
+				user.save();
+				return res.status(200).json({ message: 'Released', id: pokemonId });
+			}
+			return;
+		} catch (error) {
+			throw new Error('Error releasing pokemon ' + error);
+		}
+	}
 
-  }
+	static async getPokemonById(req: Request, res: Response) {
+		try {
+			const pokemonId = parseInt(req.params.id);
+			const pokemon = await PokemonController.pokemonService.getById(pokemonId);
+			return res.json(pokemon);
+		} catch (error) {
+			throw new Error('Error releasing pokemon ' + error);
+		}
+	}
+
+	static async searchPokemon(req: Request, res: Response) {}
 }
